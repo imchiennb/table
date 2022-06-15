bbda4a24575f418e5ea7253b98569ab011ab9665
@imchiennb <imchiennb@gmail.com>
2022-06-15 16:31:26 +0700

    Feat: update call api pass by props component

--- README.md
+++ README.md
@@ -25,9 +25,145 @@ In your project:
 ```javascript
 // Import component to your project.
 
-import { Table, useTable } from "@imchiennb/table";
+import React, { useEffect, useState } from "react";
+import { Column, Table } from "./components";
+
+type DataRow = {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  phone: number;
+  website: string;
+  address: {
+    street: string;
+  };
+};
 
 const App = () => {
-  return <Table {...props} />;
+  const columns: Column<DataRow>[] = [
+    {
+      key: "id",
+      header: "🌹 ID",
+      minWidth: 50,
+      tooltip: {
+        content: "This is an tooltip",
+      },
+      sortable: {
+        handler: (a, b) => {
+          return a.id > b.id;
+        },
+      },
+    },
+    {
+      key: "name",
+      header: "🙌 Name",
+      tooltip: {
+        content: "This is an tooltip",
+      },
+      ellipsis: {
+        line: 3,
+      },
+      render: (item) => <>{item.name}</>,
+      style: {
+        wordBreak: "break-word",
+      },
+    },
+    {
+      key: "username",
+      header: "🐱‍💻 @username",
+      span: {
+        row: 3,
+        col: 2,
+      },
+      tooltip: {
+        content: "This is an tooltip",
+      },
+      // render: (item) => <p>{JSON.stringify(item)} </p>,
+    },
+    {
+      key: "email",
+      header: "🐱‍🐉 Email",
+      tooltip: {
+        content: "This is an tooltip",
+      },
+      // render: (item) => <p>{JSON.stringify(item)} </p>,
+    },
+    {
+      key: "phone",
+      header: "🐱‍👓 Phone",
+      tooltip: {
+        content: "This is an tooltip",
+      },
+      // render: (item) => <p>{JSON.stringify(item)} </p>,
+    },
+    {
+      key: "website",
+      header: "🐱‍🚀 Website",
+      tooltip: {
+        content: "This is an tooltip",
+      },
+      // render: (item) => <p>{JSON.stringify(item)} </p>,
+    },
+    {
+      key: "address",
+      header: () => <div>Address 🌟</div>,
+      tooltip: {
+        content: "This is an tooltip",
+      },
+      render: (item) => <p>Address: {item.website} </p>,
+    },
+    {
+      key: "action",
+      header: () => <div>🐱 Action</div>,
+      minWidth: 150,
+      tooltip: {
+        content: "This is an tooltip",
+      },
+      render: (item) => (
+        <>
+          <button
+            onClick={() => {
+              alert(JSON.stringify(item));
+            }}
+          >
+            Edit
+          </button>
+          <button>Delete</button>
+        </>
+      ),
+    },
+  ];
+
+  return (
+    <>
+      <Table<DataRow>
+        columns={columns}
+        style={{ width: "100%" }}
+        fetchURL="https://jsonplaceholder.typicode.com/users"
+        handlers={{
+          onDelete: ({ params: {}, payload: {} }) => ({
+            onSuccess: (message) => {
+              console.log({ message });
+            },
+            onError: (error) => {
+              console.log({ error });
+            },
+          }),
+          onUpdate: ({ params: {}, payload: {} }) => ({
+            onSuccess: (message) => {
+              console.log({ message });
+            },
+            onError: (error) => {
+              console.log({ error });
+            },
+          }),
+        }}
+      />
+    </>
+  );
 };
+
+export default App;
+
 ```
--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,9 +1,145 @@
-import React from "react";
+import React, { ReactNode, useLayoutEffect, useState } from "react";
 
-export type TTableProps = {
-  name: string;
+export type Column<T> = {
+  key: keyof (T & { action: string });
+  header: string | (() => ReactNode);
+  span?: {
+    row?: number;
+    col?: number;
+  };
+  ellipsis?: {
+    line: number;
+  };
+  tooltip?: {
+    content: string | ReactNode;
+  };
+  icon?: {
+    before?: ReactNode;
+    after?: ReactNode;
+  };
+  style?: React.CSSProperties;
+  minWidth?: number;
+  sortable?: {
+    handler: (a: T, b: T) => boolean;
+  };
+  render?: (item: T) => ReactNode;
+  width?: number;
 };
 
-export const Table = ({ name }: TTableProps) => {
-  return <div>Table: {name}</div>;
+export type TableProps<T> = {
+  columns: Column<T>[];
+  data?: T[];
+  className?: string;
+  style?: React.CSSProperties;
+  headerAlign?: "left" | "center" | "right";
+  apiFetching?: () => Promise<T[]>;
+  handlers?: {
+    onDelete?: (props: { params?: any; payload?: any }) => {
+      onSuccess: (message: string) => void;
+      onError: (message: string) => void;
+    };
+    onUpdate?: (props: { params?: any; payload?: any }) => {
+      onSuccess: (message: string) => void;
+      onError: (message: string) => void;
+    };
+  };
+};
+
+export const Table = <T extends {}>({
+  columns,
+  className,
+  style,
+  headerAlign = "left",
+  apiFetching,
+  data: propsData,
+}: TableProps<T>) => {
+  const [data, setData] = useState<T[]>([]);
+
+  useLayoutEffect(() => {
+    const handleFetchData = async () => {
+      if (propsData) {
+        setData(propsData);
+      }
+      if (apiFetching) {
+        const data = await apiFetching();
+        setData(data);
+      }
+    };
+
+    handleFetchData();
+  }, []);
+
+  const getCellStyles = (item: Column<T>): React.CSSProperties => {
+    if (item.ellipsis && item.ellipsis.line)
+      return {
+        overflow: "hidden",
+        textOverflow: "ellipsis",
+        WebkitLineClamp: item.ellipsis.line,
+        width: item.width,
+        display: "-webkit-box",
+        WebkitBoxOrient: "vertical",
+      };
+
+    return {};
+  };
+
+  const getHeaderStyles = (): React.CSSProperties => {
+    return {
+      textAlign: headerAlign,
+    };
+  };
+  const renderHeader = () => {
+    return (
+      <tr>
+        {columns.map((column, index) => {
+          return (
+            <th
+              key={`${column.header} ${index}`}
+              style={{ ...getHeaderStyles() }}
+            >
+              {typeof column.header === "string"
+                ? column.header
+                : column.header?.()}
+            </th>
+          );
+        })}
+      </tr>
+    );
+  };
+
+  const renderBody = () => {
+    return data.map((item) => {
+      return (
+        <tr>
+          {columns.map((column) => {
+            return (
+              <td
+                style={{
+                  ...column.style,
+                  minWidth: column.minWidth,
+                }}
+              >
+                {!!column.render ? (
+                  <div style={{ ...getCellStyles(column) }}>
+                    {column.render(item)}
+                  </div>
+                ) : (
+                  <>{column.key === "action" ? null : item?.[column.key]}</>
+                )}
+              </td>
+            );
+          })}
+        </tr>
+      );
+    });
+  };
+
+  return (
+    <table className={className} style={style}>
+      {/* Render Heading */}
+      <thead>{renderHeader()}</thead>
+      {/* Render Body */}
+      <tbody>{renderBody()}</tbody>
+    </table>
+  );
 };
